9233cab4d2b4a5eea16833f32a012e9b862c91b2
Kwakuafram <kwakuafram89@gmail.com>
2022-06-28 07:42:01 -0700

    iteration

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-	if chr(i) == 'e' or chr(i) == 'q':
-		continue
-	print("{0:s}".format(chr(i)), end='')
+for i in range(ord('a'), ord('{')):
+    if i == 113 or i == 101:
+        continue
+    print("{0:s}".format(chr(i)), end='')
