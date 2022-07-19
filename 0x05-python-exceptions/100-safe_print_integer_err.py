2b5eb242a134e820b6139f829543b5c9012764d8
Kwakuafram <kwakuafram89@gmail.com>
2022-07-19 03:37:44 -0700

    My first commit

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def safe_function(fct, *args):
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
+    else:
+        return True
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
+
 def safe_print_list_integers(my_list=[], x=0):
-    num = 0
-    for i in range(num, x):
+    i = 0
+    printed = 0
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end='')
-            num += 1
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
         except (ValueError, TypeError):
-            pass
+            continue
     print()
-    return num
+    return printed
