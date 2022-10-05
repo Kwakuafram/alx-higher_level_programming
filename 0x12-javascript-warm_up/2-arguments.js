a8b3f201c911c29a9265220cfa15366078b215f6
Kwakuafram <vidaforkuo557@gmail.com>
2022-10-05 18:19:06 +0000

    Javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-/**
- * This program prints a message depending on the number of
- * arguments passed
- */
-if (process.argv.length <= 2 || !process.argv) {
-console.log('No argument');
-}
-else if (process.argv.length === 3){
-console.log('Argument found');
-}
-else {
-console.log('Arguments found');
+
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
