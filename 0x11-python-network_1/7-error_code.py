bf32a3728fedac79d409bb48ca07811fffd1b50a
Kwakuafram <vidaforkuo557@gmail.com>
2022-10-21 04:26:11 -0700

    networking

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""#7"""
+from sys import argv
+import requests
+
+
+def Error():
+    """ function
+    """
+    result = requests.get(argv[1])
+    if result.status_code > 400:
+        print("Error code: {}".format(result.status_code))
+    else:
+        print(result.text)
+
+if __name__ == "__main__":
+    Error()
